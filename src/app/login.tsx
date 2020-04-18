155c1a6da39f82ff42368b99f493081c521119d4
thr0w <josefernandotolentino@gmail.com>
2020-04-18 14:29:33 -0300

    first deploy

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -13,36 +13,36 @@ export function Login() {
       </div>
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Username</label>
+          <label className="label">email</label>
         </div>
         <div className="field-body">
           <div className="field">
             <div className="control">
-              <input className="input" name="username" id="username" type="text" placeholder="Username" autoFocus data-validate="require" disabled />
+              <input className="input" name="email" id="email" type="text" placeholder="email" autoFocus data-validate="require" disabled />
             </div>
           </div>
         </div>
       </div>
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Password</label>
+          <label className="label">Senha</label>
         </div>
         <div className="field-body">
           <div className="field">
             <div className="control has-icon has-icon-right">
-              <input className="input" type="password" name="password" id="password" placeholder="Password" data-validate="require" disabled />
+              <input className="input" type="password" name="senha" id="senha" placeholder="Senha" data-validate="require" disabled />
             </div>
           </div>
         </div>
       </div>
       <div className="field is-horizontal">
         <div className="field-label is-normal">
-          <label className="label">Confirm password</label>
+          <label className="label">Confirma senha</label>
         </div>
         <div className="field-body">
           <div className="field">
             <div className="control has-icon has-icon-right">
-              <input className="input" type="password" name="password_confirm" id="password_confirm" placeholder="Confirm password" data-validate="require" disabled />
+              <input className="input" type="password" name="senha_confirm" id="senha_confirm" placeholder="Confirma senha" data-validate="require" disabled />
             </div>
           </div>
         </div>
